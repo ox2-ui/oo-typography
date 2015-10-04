441709b1f45a955cff645040b268a33c241a09b1
Lauricio Su <me@lauricio.com>
2015-10-04 19:36:56 +0700

    updated less dependency to 2.5.0

---
+++ README.md
@@ -0,0 +1,1 @@
+### DO NOT USE for testing purposes ONLY
--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ox2:typography',
-  summary: 'Base typographic styles',
-  version: '1.0.0',
+  summary: 'TESTING_DO_NOT_USE Base typographic styles',
+  version: '1.1.0',
   git: ' /* Fill me in! */ '
 });
 
@@ -10,10 +10,10 @@ var C = 'client';
 var CS = [C, S];
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0.2.1');
+  api.versionsFrom('1.2.0.2');
   // 3rd party
   api.use([
-    'lauricio:less-autoprefixer@1.0.7'
+    'lauricio:less-autoprefixer@2.5.0_3'
     ]);
   api.addFiles('lib/oo-typography.less', C);
 });
